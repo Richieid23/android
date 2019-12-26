c86f7d13b0aaecfe1a7e8d4021813246a0fda7b7
Richieid23 <fitrarizki23@gmail.com>
2019-12-26 12:05:10 +0700

    Tugas 1

--- app/src/main/java/com/example/profilerizki/Profileku.java
+++ app/src/main/java/com/example/profilerizki/Profileku.java
@@ -1,12 +1,14 @@
 package com.example.profilerizki;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class Profileku extends AppCompatActivity {
 
@@ -15,16 +17,31 @@ public class Profileku extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileku);
 
-        ImageButton back = findViewById(R.id.btn_back);
-        back.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.btn_back){
-                    Intent kembali = new Intent(Profileku.this, MainActivity.class);
-                    startActivity(kembali);
-                }
+                finish();
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.setting){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
